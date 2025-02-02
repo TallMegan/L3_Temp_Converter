b0ae7da88dbd0bed3ff2a3425e6d511a1645e65f
Terrence <135181592+TallMegan@users.noreply.github.com>
2025-02-03 12:27:06 +1300

    Added some instructions

--- C_01_Temp_Gui_v1.py
+++ C_01_Temp_Gui_v1.py
@@ -15,8 +15,15 @@ class Converter:
         self.temp_frame.grid()
 
         self.temp_heading = Label(self.temp_frame,
-                                  text="Temperature Converter")
+                                  text="Temperature Converter", font=("Arial", "16", "bold"))
         self.temp_heading.grid(row=0)
+
+        instructions = "Please enter a temperature below and " \
+                       "then press one of the buttons to convert it from centigrade to Fahrenheit"
+        self.temp_instructions = Label(self.temp_frame, text=instructions,
+                                       wraplength=250, width=40, justify="left")
+
+        self.temp_instructions.grid(row=1)
 
 
 # main routine
