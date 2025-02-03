0f2d002245265cd0eed73c5e1360d64d0e4fcc06
Terrence <135181592+TallMegan@users.noreply.github.com>
2025-02-04 12:57:22 +1300

    Added text box. error message below text box and added the buttons

--- C_01_Temp_Gui_v1.py
+++ C_01_Temp_Gui_v1.py
@@ -25,6 +25,39 @@ class Converter:
 
         self.temp_instructions.grid(row=1)
 
+        self.temp_entry = Entry(self.temp_frame,
+                                font=("Arial", "14"))
+        self.temp_entry.grid(row=2, padx=10, pady=10)
+
+        error = "Please enter a number"
+        self.temp_error = Label(self.temp_frame, text=error,
+                                fg="#9C0000")
+        self.temp_error.grid(row=3)
+
+        # Conversion, help and history / export buttons
+        self.button_frame = Frame(self.temp_frame)
+        self.button_frame.grid(row=4)
+
+        self.to_celsius_button = Button(self.button_frame, text="To Celsius", bg="#990099",
+                                        fg="#ffffff", font=("Arial", "12", "bold"), width=12)
+
+        self.to_celsius_button.grid(row=0, column=0, padx=5, pady=5)
+
+        self.to_fahrenheit_button = Button(self.button_frame, text="To Fahrenheit", bg="#009900",
+                                           fg="#ffffff", font=("Arial", "12", "bold"), width=12)
+
+        self.to_fahrenheit_button.grid(row=0, column=1, padx=5, pady=5)
+
+        self.to_help_button = Button(self.button_frame, text="Help / Info", bg="#ff8c00",
+                                     fg="#ffffff", font=("Arial", "12", "bold"), width=12)
+
+        self.to_help_button.grid(row=1, column=0, padx=5, pady=5)
+
+        self.to_history_button = Button(self.button_frame, text="History / Export", bg="#990019",
+                                        fg="#ffffff", font=("Arial", "12", "bold"), width=12)
+
+        self.to_history_button.grid(row=1, column=1, padx=5, pady=5)
+
 
 # main routine
 
